ac3ecbd1361439d5f7be38778e774e956f733340
ramakrishnareddya <ramakrishna.aamuru@gamil.com>
2024-08-09 11:38:04 +0530

    added new class in method package

---
+++ src/methods/MethodPerameter.java
@@ -0,0 +1,14 @@
+package methods;
+
+public class MethodPerameter {
+	public static void main(String[] args) {
+		myParameterMethod("nani");
+		myParameterMethod("Rk");
+		
+	}
+	
+	static void myParameterMethod (String fname) {
+		System.out.println(fname + "Reddy");
+	}
+
+}
