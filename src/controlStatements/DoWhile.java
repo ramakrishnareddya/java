8191ba15c795dd6ab645c8e7d9e4cc67172d5d70
ramakrishnareddya <ramakrishna.aamuru@gamil.com>
2024-08-23 09:30:01 +0530

    new class

---
+++ src/controlStatements/DoWhile.java
@@ -0,0 +1,16 @@
+package controlStatements;
+
+public class DoWhile {
+
+	public static void main(String[] args) {
+
+
+		int i = 10;
+		
+		do {
+			System.out.println(i);
+			i++;
+		} while (i<=100);
+	}
+
+}
