2948687ce388373822033af187709fb244f24853
ramakrishnareddya <ramakrishna.aamuru@gamil.com>
2024-08-23 12:54:57 +0530

    new commit

--- src/strings/ReverseString.java
+++ src/strings/ReverseString.java
@@ -1,10 +1,20 @@
 package strings;
 
+import java.util.Scanner;
+
 public class ReverseString {
+	
 	public static void main(String[] args) {
-		String name ="ramakrishna";
-		int length=name.length();
+		
+		String name;
+		
+		try (Scanner rev = new Scanner(System.in)){
+			System.out.println("Enter String");
+			name = rev.nextLine();
+		}
+	
 		String reverse="";
+		int length=name.length();
 		
 		for(int i=length-1; i>=0; i--) {
 			reverse=reverse+name.charAt(i);
